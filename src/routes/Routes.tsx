35501f7add25bf82de8b9c0b8cfb6edc8331da25
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-06 14:00:03 -0600

    feat(route): ruta administrador

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { TicketsForm } from "../pages/TicketsForm/TicketsForm";
+import { AdminTickets } from "../pages/Administrator/AdminTickets";
 
 export const MyRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<TicketsForm />} />
+      <Route path="/administrador" element={<AdminTickets />} />
     </Routes>
   );
 };
