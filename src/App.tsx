ee55ec4a306e36d254a3fd9e9605863b9cb5df89
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-06 12:47:36 -0600

    feat(app): configuracion de rutas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
 import { Navbar } from "./components/Navbar/Navbar";
+import { MyRoutes } from "./routes/Routes";
 
 export const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       <Navbar />
-    </div>
+      <main>
+        <MyRoutes />
+      </main>
+    </BrowserRouter>
   );
 };
