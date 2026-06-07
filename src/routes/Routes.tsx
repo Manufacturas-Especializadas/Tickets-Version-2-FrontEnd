1f5b6f0bd65b3fd8a97ccf2b1db57a4f7f6c64d4
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-07 14:42:08 -0600

    feat(route): ruta protegida

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,13 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import { TicketsForm } from "../pages/TicketsForm/TicketsForm";
 import { AdminTickets } from "../pages/Administrator/AdminTickets";
 import { Login } from "../pages/Auth/Login";
+import { ProtectedRoute } from "../components/Auth/ProtectedRoute";
 
 export const MyRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<TicketsForm />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/administrador" element={<AdminTickets />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path="/administrador" element={<AdminTickets />} />
+      </Route>
     </Routes>
   );
 };
