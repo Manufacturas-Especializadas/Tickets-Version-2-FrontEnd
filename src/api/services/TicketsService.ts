a1ad8d9e9369926d1d18a50e60f3ca1bab57fcaf
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-07 12:20:06 -0600

    feat(service): metodo para obtener los detalles por id

--- src/api/services/TicketsService.ts
+++ src/api/services/TicketsService.ts
@@ -1,13 +1,22 @@
 import { API_CONFIG } from "../../config/api";
-import type { AllTickets, UpdateTicket } from "../../types/Types";
+import type {
+  AllTickets,
+  DetailsTicket,
+  UpdateTicket,
+} from "../../types/Types";
 import { apiClient } from "../client";
 
 class TicketsService {
   private getAllEndpoint = API_CONFIG.endpoint.tickets.all;
+  private getByEndpoint = API_CONFIG.endpoint.tickets.byId;
   private updateEndpoint = API_CONFIG.endpoint.tickets.update;
 
   async getAll(): Promise<AllTickets[]> {
     return apiClient.get<AllTickets[]>(this.getAllEndpoint);
+  }
+
+  async getBy(id: number): Promise<DetailsTicket> {
+    return apiClient.get<DetailsTicket>(`${this.getByEndpoint}${id}`);
   }
 
   async update(id: number, payload: UpdateTicket): Promise<void> {
