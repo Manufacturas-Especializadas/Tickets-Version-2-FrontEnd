c64c2f41d214902eeda6cd6cfb34683b3453ef0a
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-07 14:25:37 -0600

    feat(app): toast sonner

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter } from "react-router-dom";
 import { Navbar } from "./components/Navbar/Navbar";
 import { MyRoutes } from "./routes/Routes";
 import { AuthProvider } from "./context/AuthContext";
+import { Toaster } from "sonner";
 
 export const App = () => {
   return (
     <AuthProvider>
       <BrowserRouter>
+        <Toaster richColors position="top-right" />
         <Navbar />
         <main>
           <MyRoutes />
