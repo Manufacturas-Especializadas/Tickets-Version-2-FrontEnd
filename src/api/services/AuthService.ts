9d39080cfb4349cee34bf85f42c246f85045524b
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-07 14:06:56 -0600

    feat(service): metodo login

--- src/api/services/AuthService.ts
+++ src/api/services/AuthService.ts
@@ -1,3 +1,13 @@
-class AuthService {}
+import { API_CONFIG } from "../../config/api";
+import type { Login } from "../../types/Types";
+import { apiClient } from "../client";
+
+class AuthService {
+  private loginEndpoint = API_CONFIG.endpoint.Auth.login;
+
+  async login(payload: Login): Promise<void> {
+    return apiClient.post<void>(this.loginEndpoint, payload);
+  }
+}
 
 export const authService = new AuthService();
