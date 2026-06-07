f394c53b2fa2da35ce83d45a11f6293e789bd951
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-07 11:38:41 -0600

    feat(service): metodo para obtener todos los tickets

--- src/api/services/TicketsService.ts
+++ src/api/services/TicketsService.ts
@@ -1,3 +1,13 @@
-class TicketsService {}
+import { API_CONFIG } from "../../config/api";
+import type { AllTickets } from "../../types/Types";
+import { apiClient } from "../client";
+
+class TicketsService {
+  private getAllEndpoint = API_CONFIG.endpoint.tickets.all;
+
+  async getAll(): Promise<AllTickets[]> {
+    return apiClient.get<AllTickets[]>(this.getAllEndpoint);
+  }
+}
 
 export const ticketsService = new TicketsService();
