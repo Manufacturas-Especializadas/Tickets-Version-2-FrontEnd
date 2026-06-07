62fdfb76ce5d3e6582e9c0682d8dcd7196289ccf
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-07 11:52:39 -0600

    feat(service): actualizar un ticket

--- src/api/services/TicketsService.ts
+++ src/api/services/TicketsService.ts
@@ -1,12 +1,19 @@
 import { API_CONFIG } from "../../config/api";
-import type { AllTickets } from "../../types/Types";
+import type { AllTickets, UpdateTicket } from "../../types/Types";
 import { apiClient } from "../client";
 
 class TicketsService {
   private getAllEndpoint = API_CONFIG.endpoint.tickets.all;
+  private updateEndpoint = API_CONFIG.endpoint.tickets.update;
 
   async getAll(): Promise<AllTickets[]> {
     return apiClient.get<AllTickets[]>(this.getAllEndpoint);
+  }
+
+  async update(id: number, payload: UpdateTicket): Promise<void> {
+    const url = `${this.updateEndpoint}/${id}/resolve`;
+
+    return apiClient.put<void>(url, payload);
   }
 }
 
