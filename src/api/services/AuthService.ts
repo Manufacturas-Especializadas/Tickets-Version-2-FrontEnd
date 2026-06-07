868c36480ab59dc5519ae96f81283c84ba7c6753
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-07 14:11:18 -0600

    fix(service): devuelve auth response

--- src/api/services/AuthService.ts
+++ src/api/services/AuthService.ts
@@ -1,12 +1,12 @@
 import { API_CONFIG } from "../../config/api";
-import type { Login } from "../../types/Types";
+import type { AuthResponse, Login } from "../../types/Types";
 import { apiClient } from "../client";
 
 class AuthService {
   private loginEndpoint = API_CONFIG.endpoint.Auth.login;
 
-  async login(payload: Login): Promise<void> {
-    return apiClient.post<void>(this.loginEndpoint, payload);
+  async login(payload: Login): Promise<AuthResponse> {
+    return apiClient.post<AuthResponse>(this.loginEndpoint, payload);
   }
 }
 
