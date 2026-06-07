9cc0d3d4d1c59c2ee8b3a4f2bd9de03c99ea264a
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-07 14:21:58 -0600

    feat(route): ruta login

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { TicketsForm } from "../pages/TicketsForm/TicketsForm";
 import { AdminTickets } from "../pages/Administrator/AdminTickets";
+import { Login } from "../pages/Auth/Login";
 
 export const MyRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<TicketsForm />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/administrador" element={<AdminTickets />} />
     </Routes>
   );
